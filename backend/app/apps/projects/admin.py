0b309ea73792f7727663bac15fb4694960928f6f
Paulo Martins <paulolm20@gmail.com>
2026-06-20 22:01:18 +0100

    feat: add project model

--- backend/app/apps/projects/admin.py
+++ backend/app/apps/projects/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Project
+
+
+@admin.register(Project)
+class ProjectAdmin(admin.ModelAdmin):
+    list_display = ("id", "name", "workspace", "created_at")
+    search_fields = ("name", "workspace__name")
+    list_filter = ("workspace",)
--- backend/app/apps/projects/models.py
+++ backend/app/apps/projects/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
-# Create your models here.
+from apps.workspaces.models import Workspace
+
+
+class Project(models.Model):
+    workspace = models.ForeignKey(
+        Workspace,
+        on_delete=models.CASCADE,
+        related_name="projects",
+    )
+
+    name = models.CharField(max_length=100)
+    description = models.TextField(blank=True)
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.name
