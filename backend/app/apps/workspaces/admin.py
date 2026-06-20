1f3f5fb87b828e2402948517236e75c3ac9b1f8e
Paulo Martins <paulolm20@gmail.com>
2026-06-20 01:08:28 +0100

    feat: add workspace model

--- backend/app/apps/workspaces/admin.py
+++ backend/app/apps/workspaces/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Workspace
+
+
+@admin.register(Workspace)
+class WorkspaceAdmin(admin.ModelAdmin):
+    list_display = ("id", "name", "owner", "created_at")
+    search_fields = ("name",)
--- backend/app/apps/workspaces/models.py
+++ backend/app/apps/workspaces/models.py
@@ -1,3 +1,19 @@
+from django.conf import settings
 from django.db import models
 
-# Create your models here.
+
+class Workspace(models.Model):
+    owner = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+        related_name="owned_workspaces"
+    )
+
+    name = models.CharField(max_length=100)
+    description = models.TextField(blank=True)
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.name
